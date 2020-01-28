e12d8328f9da4ab858d96e2f0acc4545a7850e6d
DracoMindz <DracoMindz@github.com>
2020-01-28 20:26:38 +0000

    shape pf matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -6,10 +6,9 @@ def matrix_shape(matrix):
     """Finds dimensions of matrix"""
     if len(matrix) == 0:
         return [0]
-    size = [len(matrix)]
-    if type(matrix) is int:
-        return
-    else:
+    m = matrix[:]
+    size = [len(m)]
+    while type(matrix[0]) is list:
         size.append(len(matrix[0]))
         matrix = matrix[0]
-        return size
+    return size
