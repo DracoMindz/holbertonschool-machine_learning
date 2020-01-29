f0eef6de174b9ce1ea80b5126eb95680539b6e69
DracoMindz <DracoMindz@github.com>
2020-01-29 11:54:31 +0000

    executable

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-
-
 """function concatenates two matrices along a specific axis"""
+
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices along an axis"""
