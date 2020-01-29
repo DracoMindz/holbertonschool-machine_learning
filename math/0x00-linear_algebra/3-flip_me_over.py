7982bdd2d244fbc118b21efd51b0a5612152e3c8
DracoMindz <DracoMindz@github.com>
2020-01-29 03:44:47 +0000

    executable

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def matrix_transpose(matrix):
+
+    """transpose matrix"""
+    m = []
+    for i in range(len(matrix[0])):
+        mx = []
+        for j in range(len(matrix)):
+            mx.append(matrix[j][i])
+        m.append(mx)
+    return(m)
