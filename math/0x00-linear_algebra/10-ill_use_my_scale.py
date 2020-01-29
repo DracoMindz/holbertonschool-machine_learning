09a685949fdd1f73c5c58c079ca85c2418fcdfca
DracoMindz <DracoMindz@github.com>
2020-01-29 23:15:21 +0000

    shape of matrix

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """calculates the shap of matrix"""
 
-import numpy as np
-
 
 def np_shape(matrix):
-    # calculate shape of numpy.ndarray
-    return(matrix.shape)
+    # return shape of numpy.ndarray as tuple
+    return (matrix.shape)
