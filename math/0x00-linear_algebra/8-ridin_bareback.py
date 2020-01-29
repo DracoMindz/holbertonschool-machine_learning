8ec1578cdbebe1b106b515983f057d072a438689
DracoMindz <DracoMindz@github.com>
2020-01-29 14:01:04 +0000

    matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,14 +4,16 @@
 
 def mat_mul(mat1, mat2):
     """multiply matrices"""
+    newMatrix = []
+    # length of rows must be equal
     if len(mat1[0]) != len(mat2):
         return None
-
-    newMatrix = [[] for i in mat1]
     for i in range(len(mat1)):
+        mRow = []
         for j in range(len(mat2[0])):
             result = 0
             for m in range(len(mat1[0])):
-                result += mat1[i][m] * mat2[m][i]
-                newMatrix[i].append(result)
+                result += mat1[i][m] * mat2[m][j]
+            mRow.append(result)
+        newMatrix.append(mRow)
     return(newMatrix)
