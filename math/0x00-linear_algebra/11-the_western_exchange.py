825a246a27a2d7e7ab0f9c753f7937d91428d8d9
DracoMindz <DracoMindz@github.com>
2020-01-29 23:46:46 +0000

    transpose matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """transpose matrix and return new matrix"""
+    newArr = []
+
+    newArr.append(matrix.transpose())
+    return newArr
