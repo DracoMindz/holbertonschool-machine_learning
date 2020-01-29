42f89976d0b565a3bacf2575b2648e326a9a49fb
DracoMindz <DracoMindz@github.com>
2020-01-29 11:02:36 +0000

    concatenate arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+"""function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """concatenate two arrays"""
+    newArr = []
+    newArr = arr1 + arr2
+    return(newArr)
