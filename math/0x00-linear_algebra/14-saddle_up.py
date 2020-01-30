692b8b1ed513963d6f6a316a42b3ffb38f738fa1
DracoMindz <DracoMindz@github.com>
2020-01-30 16:07:05 +0000

    mult matrix

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function performs matric multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """matrix multiplication"""
+    return np.matmul(mat1, mat2)
