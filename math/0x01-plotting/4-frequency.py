6bd0aa17f5168cbf83b94519e74b61ea486f418a
DracoMindz <DracoMindz@github.com>
2020-02-02 03:03:26 +0000

    histogram

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
-x = student_grades
-y
-ax = plt.subplot(111)
-w = 0.1
-ax.bar(x, y, width=w, color='b', align='edge')
+
+"""data format"""
+bin_edges = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+plt.hist(students_grades, align='mid', bins=bin_edges, edgecolor='black')
+
 
 """graph design"""
-plt.axis([0, 100, 0, 30])
 plt.ylabel('Number of Students')
 plt.xlabel('Grades')
-plt.title('PROJECT A')
+plt.title('Project A')
+plt.xlim(0, 100)
+plt.xticks(bin_edges)
+plt.ylimit(0, 30)
 plt.show()
