adecaab61807353236bec91fb716893d1b900f09
DracoMindz <DracoMindz@github.com>
2020-02-03 20:37:16 +0000

    derivative of poly list

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pyhton3
+"""function calculates derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """function calculates derivative of a polnomial"""
+
+    coIndex = 0
+    dePoly = []
+
+    if len(poly) == 0:
+        return None
+
+    for p, c in enumerate(poly[1:]):
+        if type(c) is not int or type(c) is float:
+            return None
+        if c != 0:
+            coIndex = p
+        dePoly.append(c * (p + 1))
+    if coIndex == 0:
+        return [0]
+
+    return dePoly[:coIndex + 1]
