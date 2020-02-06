40b7c339737f475ff0e4a7cca155658a17e03915
DracoMindz <DracoMindz@github.com>
2020-02-06 16:41:44 +0000

    verification

--- math/0x01-plotting/1-scatter.py
+++ math/0x01-plotting/1-scatter.py
@@ -8,7 +8,8 @@ np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
 
-plt.scatter(x, y)
+plt.plot(x, y, 'mo')
 plt.xlabel('Height (in)')
 plt.ylabel('Weight (lbs)')
+plt.title('Men\'s Height vs Weight')
 plt.show()
