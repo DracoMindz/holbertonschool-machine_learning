5be2a9cbf1abcd42c33a727d99d775a286fd232f
DracoMindz <DracoMindz@github.com>
2020-02-06 20:12:07 +0000

    poisson data

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Create class to represent Poisson distribution"""
+
+
+class Poisson:
+    """data distrubution"""
+    def __init__(self, data=None, lambtha=1.):
+        """initialize data"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
