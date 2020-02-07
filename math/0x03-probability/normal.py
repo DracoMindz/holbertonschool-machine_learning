38e4a42ae7c6a0c1b38823084a99f84cc0777c35
DracoMindz <DracoMindz@github.com>
2020-02-07 06:35:35 +0000

    Normal

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -20,3 +20,11 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = float((sum([(x - self.mean) ** 2 for x in data])
                                  / (len(data))) ** .5)
+
+    def z_score(self, x):
+        """Calculates z-score of x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates x-value of z-score"""
+        return z * self.stddev + self.mean
