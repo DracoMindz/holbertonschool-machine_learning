ec062ea0dba45bcb461792d4f1e10d16a80c8ee5
DracoMindz <DracoMindz@github.com>
2020-02-07 10:03:26 +0000

    Normal

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -28,3 +28,9 @@ class Normal:
     def x_value(self, z):
         """Calculates x-value of z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates value of pdf at x"""
+        return (pow(2.7182818285, ((x - self.mean) ** 2 /
+                                   (-2 * self.stddev ** 2))) /
+                (2 * 3.1415926536 * self.stddev ** 2) ** .5)
