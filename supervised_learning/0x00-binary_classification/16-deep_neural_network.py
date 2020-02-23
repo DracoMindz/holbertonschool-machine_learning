8c3833be564b1af4f5d49b303af3ef5880ec50cc
DracoMindz <DracoMindz@github.com>
2020-02-23 00:51:45 +0000

    DNN Class

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -14,20 +14,23 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         """layers list reping num nodes in each layer"""
-        if type(layers) is not (list) or len(layers) < 1:
+        if type(layers) is not (list) or len(layers) <= 0:
             raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.nx = nx
         self.cache = {}
+        self.weights = {}
         for i_lyr in range(self.L):
+            mWts = "W" + str(i_lyr + 1)
+            mB = "b" + str(i_lyr + 1)
             if type(layers[i_lyr]) is not (int) or layers[i_lyr] < 1:
                 raise TypeError("layers must be a list of positive integers")
-            self.weights = {"W1": np.random.randn(layers[0], nx) *
-                            np.sqrt(2 / nx),
-                            "b1": np.zeros((layers[i_lyr], 1))}
-            if type(layers[i_lyr]) is (int) and layers[i_lyr] > 1:
-                mWts = "W" + str(i_lyr + 1)
-                mB = "b" + str(i_lyr + 1)
+            self.weights[mB] = np.zeros((layers[i_lyr], 1))
+            if i_lyr == 0:
                 self.weights[mWts] = (np.random.randn(layers[i_lyr - 1], nx)
                                       * np.sqrt(2 / nx))
+            elif i_lyr >= 1:
+                self.weights[mWts] = (np.random.randn(layers[i_lyr],
+                                      layers[i_lyr - 1])
+                                      * np.sqrt(2 / layers[i_lyr - 1]))
                 self.weights[mB] = np.zeros((layers[i_lyr - 1], 1))
