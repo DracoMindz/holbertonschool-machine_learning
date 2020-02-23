b3464911579b08ba45322302e372a6c60329e349
DracoMindz <DracoMindz@github.com>
2020-02-23 23:17:43 +0000

    One Hot One Cold

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -11,10 +11,14 @@ def one_hot_encode(Y, classes):
     m is num of examples
     classes is max num classes found in Y
     """
-    b = np.zeros((Y.shape[0], classes))
-    try:
+    if len(Y) == 0:
+        return None
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int or classes <= np.max(Y):
+        return None
+    else:
+        b = np.zeros((classes, Y.shape[0]))
         for cl, m in enumerate(Y):
             b[m][cl] = 1
         return b
-    except Exception:
-        return None
