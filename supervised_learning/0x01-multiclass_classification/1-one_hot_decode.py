c7a776bdec810a86f5e06717d9e8ae63e4c13fa7
DracoMindz <DracoMindz@github.com>
2020-02-24 04:40:56 +0000

    one hot decode

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -10,12 +10,15 @@ def one_hot_decode(one_hot):
     (classes, m)
     """
 
+    
     if len(one_hot.shape) != 2 or len(one_hot) == 0:
         return None
+    """
     if type(one_hot) is not np.ndarray:
         return None
     if not np.all((one_hot == 0) | (one_hot == 1)):
         return None
+    """
     try:
         return np.argmax(one_hot, axis=0)
     except Exception:
