ad6ac9b593d384f58e2fa2ebbbfdb809b572d1fa
DracoMindz <DracoMindz@github.com>
2020-02-26 04:58:32 +0000

    closer to one

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -9,7 +9,6 @@ def calculate_accuracy(y, y_pred):
     y is a placeholder for the labels of the input data
     y_pred is a tensor containing the network’s predictions
     """
- 
 
     prediction = tf.argmax(y_pred, 1)
     results = tf.argmax(y, 1)
