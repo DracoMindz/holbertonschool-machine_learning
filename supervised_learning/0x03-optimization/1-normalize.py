6e8592c81653db7df29cd5d2935bd3dfc7edc81f
DracoMindz <DracoMindz@github.com>
2020-03-02 10:06:03 +0000

    Normalize inputs/matrix

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-"""
+"""function normalizes(standardizes) a matrix"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def normalize(X, m, s):
+    """
+    X is numpy.ndarray of shape (d, nx) to normalize
+    m is numpy.ndarray of shape (nx,) contains features of X
+    s is numpy.ndarray of shape (nx,) contains Std Dev of X
+    d id the num of data points
+    nx is num of features
+    """
+    return (X - m) / s
