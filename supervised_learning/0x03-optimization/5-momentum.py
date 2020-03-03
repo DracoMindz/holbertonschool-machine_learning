82a85edc2081ec205bfc999e888e25043d5ff18b
DracoMindz <DracoMindz@github.com>
2020-03-03 17:27:22 +0000

    updates Variable

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+function updates variable using grad descent
+with momentum optimization algoritm
+"""
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    alpha: learning rate
+    beta1: momentum weight
+    var: numpy.ndarray containing variable to update
+    grad: numpy.ndarray containing gradient of var
+    v: previous first moment of var
+    """
+    V_t = beta * v + (1 - beta) * grad
+    W_var = var - alpha * V_t
+    return W_var, V_t
