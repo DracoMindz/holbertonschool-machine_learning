88b517c5cfdbb962de3be414faf5f0c38e10cb86
DracoMindz <DracoMindz@github.com>
2020-03-05 22:00:37 +0000

    create matrix

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+creates a confusion matrix
+"""
+
+import numpy as py
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    labels: one-hot numpy.ndarray shape (m, classes)
+            containins correct labels each data point
+    logits: one-hot numpy.ndarray shape (m, classes)
+            containing the predicted labels
+    m: num data points
+    classes: numb classes
+    """
+    return np.dot(labels.T, logits)
