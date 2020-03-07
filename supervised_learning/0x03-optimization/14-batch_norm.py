271c3f18fc428d0b1aa9ccc1f79d2aff2508b0eb
DracoMindz <DracoMindz@github.com>
2020-03-07 06:03:09 +0000

    changed get var

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -3,7 +3,7 @@
 function creates  a batch normalization layer for NN
 in tensorflow
 """
-
+import numpy
 import tensorflow as tf
 
 
@@ -14,17 +14,18 @@ def create_batch_norm_layer(prev, n, activation):
     activation: activation function used on output of layer
     """
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    m = tf.layers.Dense(units=n, activation=None,
-                          kernel_initializer=init)
-    beta = (tf.nn.get_variable("beta", [n],
+    mm = tf.layers.Dense(units=n, activation=None,
+                         kernel_initializer=init)
+    batch_mean, batch_var = tf.nn.moments(mm(prev), axes=[0])
+
+    beta = (tf.get_variable("beta", [n],
                                initializer=tf.zeros_initializer(),
                                trainable=True))
-    gamma = (tf.nn.get_variable("gamma", [n],
+    gamma = (tf.get_variable("gamma", [n],
                                 initializer=tf.ones_initializer(),
                                 trainable=True))
-    batch_mean, batch_var = tf.nn.moments(m(prev), axes=[0])
-    norm_layer = (tf.nn.batch_normalization(m(prev), mean=batch_mean,
-                                            variance=batch_var, offset=beta,
-                                            scale=gamma,
-                                            variance_epsilon=1e-8))
-    return activation(norm_layer)
+    normlayer = (tf.nn.batch_normalization(mm(prev), mean=batch_mean,
+                                           variance=batch_var, offset=beta,
+                                           scale=gamma,
+                                           variance_epsilon=1e-8))
+    return activation(normlayer)
