b5acef0521f4811018876b24d9723e1a5d4d9551
DracoMindz <DracoMindz@github.com>
2020-03-07 04:21:34 +0000

    read the quest? closely

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -13,6 +13,19 @@ def create_batch_norm_layer(prev, n, activation):
     n: num of nodes in layer to be created
     activation: activation function used on output of layer
     """
-    norm_layer = tf.nn.batch_normalization(prev, n)
+    init = (tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"))
+    mm = (tf.layers.Dense(units=n, activation=None,
+                          kernel_initializer=init))
+    gamma = (tf.nn.get_variable("gamma", [n],
+                                initializer=tf.ones_initializer(),
+                                trainable=True))
+    beta = (tf.nn.get_variable("beta", [n],
+                               initializer=tf.zeros_initializer(),
+                               trainable=True))
+    batch_mean, batch_var = (tf.nn.moments(x(prev), axes=0))
+    norm_layer = (tf.nn.batch_normalization(x(prev), mean=batch_mean,
+                                            variance=batch_var, offset=beta,
+                                            scale=gamma,
+                                            variance_epsilon=1e-8))
     act_norm_layer = activation(norm_layer)
     return act_norm_layer
