cff3632bd5057c5a0e7418377cd5be36ade9e41c
DracoMindz <DracoMindz@github.com>
2020-03-16 08:19:59 +0000

    keras with Adam

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Adam optimization for keras model
+"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Adam optimizer for keras NN
+    """
+    model.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
+                                              beta_1=beta1,
+                                              beta_2=beta2),
+                  loss='categorical_crossentrophy',
+                  metrics=['accuracy'])
