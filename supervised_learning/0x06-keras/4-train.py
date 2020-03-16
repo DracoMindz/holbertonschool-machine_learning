7ac4d4112b8c33859a45306d7b6ecad2d59dd6a8
DracoMindz <DracoMindz@github.com>
2020-03-16 19:43:10 +0000

    keras Train Model

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+function trains a model using mini-batch gradient descent
+"""
+import numpy as np
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """
+    network: model to train
+    data: numpy.ndarray w/ shape (m, nx) contains input data
+    labels: one-hot numpy.ndarray w/ shape (m, classes) cntains labels of data
+    batch_size: size of the batch used for mini-batch grad descent
+    epochs: num passes though data for mini-batch grad descent
+    verbose: boolean determines if output is printed during training
+    shuffle: boolean determines to shuffle batches every epoch
+             set default to false
+    Return: History object generated after  training the model
+    """
+    History = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
+                          verbose=verbose, shuffle=shuffle)
+    return(History)
