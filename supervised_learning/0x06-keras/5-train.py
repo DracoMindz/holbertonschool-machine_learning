32c18b20aec1380778b4315c318cddd5a09fa4b8
DracoMindz <DracoMindz@github.com>
2020-03-16 19:59:37 +0000

    keras Train/Validate Model

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,19 @@
+#!/ usr/bin/env python3
+"""
+function to analyze validation data
+"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    validation_data is the data to validate the model
+    if not None
+    """
+
+    history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
+                          shuffle=shuffle, verbose=verbose,
+                          validation_data=validation_data)
+    return history
