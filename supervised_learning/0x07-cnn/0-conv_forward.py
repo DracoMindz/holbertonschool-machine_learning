64887353828db53ad024c473d91b167bb21956f4
DracoMindz <DracoMindz@github.com>
2020-03-25 23:12:16 +0000

    Conv FP

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -34,22 +34,46 @@ def conv_forward(A_prev, W, b, activation,
        sh: stride for height
        sw: stride for width
     """
-    m = A_prev.shape[0]
-    h_prev = A_prev.shape[1]
-    w_prev = A_prev.shape[2]
-    c_prev = A_prev.shape[3] = W.shape[2] = b.shape[3]
-    kh = W.shape[0]
-    kw = W.shape[1]
-    c_new = W.shape[3]
-    ph = padding[0]
-    pw = padding[1]
+    # retrive dimensions
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
     sh = stride[0]
     sw = stride[1]
 
+    # calculate padding for 'valid' and 'same'
     if padding == 'valid':
-        ph == 0
-        pw == 0
+        ph = 0
+        pw = 0
     if padding == 'same':
-        ph =
-        pw =
-    
+        ph = np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2)
+        ph = int(ph)
+        pw = np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2)
+        pw = int(pw)
+
+    # compute output dimensions
+    outputH = int(((h_prev - kh + (2 * ph)) / sh) + 1)
+    outputW = int(((w_prev - kw + (2 * pw)) / sw) + 1)
+
+    # initialize output with zeros
+    outputFP = np.zeros((m, outputH, outputW, c_new))
+
+    # Padding
+    padImages = np.pad(A_prev, [(0, 0), (ph, ph), (pw, pw), (0, 0)],
+                       mode='constant', constant_values=0)
+
+    # vectorize
+    padImagesVector = np.arange(0, m)
+
+    # looping engine: vertical and horizontal
+    for i in range(outputH):
+        for j in range(outputW):
+            for k in range(c_new):
+                i_s = i * sh
+                j_s = j * sw
+                outputFP[padImagesVector, i, j, k] = activation((
+                    np.sum(np.multiply(padImages[
+                        padImagesVector,
+                        i_s: i_s + kh, j_s: j_s + kw],
+                                       W[:, :, :, k]),
+                           axis=(1, 2, 3))) + b[0, 0, 0, k])
+    return outputFP
