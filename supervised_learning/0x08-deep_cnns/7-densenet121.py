97f91c122c3ab3304717a4a641d7cc61ceada48b
DracoMindz <DracoMindz@github.com>
2020-04-02 15:51:36 +0000

    dense/transition layers

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -20,14 +20,15 @@ def densenet121(growth_rate=32, compression=1.0):
     Returns: keras model
     """
 
-    inputData = K.layers.Input(shape=(224, 224, 3))
+    inputData = K.Input(shape=(224, 224, 3))
     init = K.initializers.he_normal(seed=None)
     bNorm = K.layers.BatchNormalization()(inputData)
     act = K.layers.Activation('relu')(bNorm)
     conv = K.layers.Conv2D(filters=64,
                            kernel_size=7,
                            strides=2,
-                           padding='same')(act)
+                           padding='same',
+                           kernel_initializer=init)(act)
     pool_1 = K.layers.MaxPooling2D(pool_size=3,
                                    strides=2,
                                    padding='same')(conv)
@@ -43,7 +44,7 @@ def densenet121(growth_rate=32, compression=1.0):
     output8, nbFilters8 = dense_block(output7, nbFilters7, growth_rate, 16)
 
     avgPool = K.layers.AveragePooling2D(pool_size=7,
-                                        strides=None,
+                                        strides=7,
                                         padding='same')(output8)
     outputs = K.layers.Dense(units=1000,
                              activation='softmax',
