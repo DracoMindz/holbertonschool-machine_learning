452b9404ee7d75bdc99e35152356235fc96f9350
DracoMindz <DracoMindz@github.com>
2020-04-18 00:19:00 +0000

    yolo

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -3,26 +3,26 @@
 Class constructor
 """
 import tensorflow as tf
-import tensorflow.Keras as K
+import tensorflow.keras as K
 
 
-def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-    """
-    model_path: path to Darknet Keras model
-    classes_path: path to list of class names for Darnet model
-                  list in order of index
-    class_t: float = box score threshold for inital filtering
-    nms_t: float for IoU threshold for non-max suppression
-    anchors: numpy.darray shape(outputs, anchor_boxes, 2)
-             contains all anchor boxes
-    ouputs: num Darknet Model outputs
-    anchor_boxes: num anchor boxes used for each prediction
-    2 -> [anchor_box_width, anchor_box_height]
-    """
+class Yolo:
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        model_path: path to Darknet Keras model
+        classes_path: path to list of class names for Darnet model
+                      list in order of index
+        class_t: float = box score threshold for inital filtering
+        nms_t: float for IoU threshold for non-max suppression
+        anchors: numpy.darray shape(outputs, anchor_boxes, 2)
+                 contains all anchor boxes
+        ouputs: num Darknet Model outputs
+        anchor_boxes: num anchor boxes used for each prediction
+        2 -> [anchor_box_width, anchor_box_height]
+        """
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as classFile:
             self.class_names = [line.strip() for line in classFile]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-
