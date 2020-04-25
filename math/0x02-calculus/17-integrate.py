4bc976e8b4a4f46c318b84d6b3c311a13da3c2c4
DracoMindz <DracoMindz@github.com>
2020-04-25 16:25:34 +0000

    tested & changed dePoly

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -8,23 +8,29 @@ def poly_integral(poly, C=0):
     coIndex = 0
     dePoly = []
 
-    if len(poly) == 0 or len(poly) < 1:
+    try:
+        if len(poly) < 1:
+            return None
+    except TypeError:
         return None
     """code checking if C is int"""
-    if type(C) is not int or type(C) is float:
+    if not (type(C) is int or type(C) is float):
         return None
     coIndex = 0
     for p, coef in enumerate(poly):
-        if type(coef) is not int or type(coef) is float:
+        if not (type(coef) is int or type(coef) is float):
             return None
         if coef != 0:
             coIndex = p + 1
+            # print("{}, {}".format(coef, coIndex))
             # change code to C + integral of poly
-        for pow, coef in enumerated(poly):
-            i_pow = (pow + 1)
-    dePoly = ([C] + (coef_whole(coef / i_pow))
+    # for pow, coef in enumerate(poly):
+        # i_pow = coef_whole(coef / (pow + 1))
+    # dePoly = ([C] + [i_pow])
+    dePoly = [C] + [coef_whole(coef / (exp + 1))
+                    for exp, coef in enumerate(poly)]
+    #print("{}".format(dePoly))
     return dePoly[:coIndex + 1]
-
 
 
 def coef_whole(num):
