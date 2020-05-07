cf731137c324d35d9ae2f9a831d42fcf5bc040a6
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-07 02:13:50 -0400

    use eigenvalues

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the definiteness of a matrix
+"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    calculates definiteness of a matrix
+    :param matrix: list of lists
+    :return: the string: Positive definite,
+    Positive semi-definite, Negative semi-definite,
+    Negative definite, or Indefinite
+    """
+
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) != 2:
+        return None
+    if (matrix.shape[0] != matrix.shape[1]):
+        return None
+    definite = (np.linalg.eigvals(matrix))
+
+    if all(definite > 0):
+        return "Positive definite"
+    if all(definite < 0):
+        return "Negative definite"
+    if any(definite > 0) and any(definite == 0):
+        return "Positive semi-definite"
+    if any(definite < 0) and any(definite == 0):
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
