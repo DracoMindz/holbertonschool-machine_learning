468e774c88346bbcc8d6d4445e1aa6161cf9cec4
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-18 18:34:07 -0400

    interection

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -20,16 +20,23 @@ def intersection(x, n, P, Pr):
     if not isinstance(n, int) or n < 1:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that"
-                         "is greater than or equal to 0")
+        m = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(m)
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if np.min(P) < 0 or np.amax(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
 
-    for val in P:
-        if val > 1 or val < 0:
-            raise ValueError("All values in P must be in the range [0, 1]")
+    if not isinstance(Pr, np.ndarray) or (P.shape) != (Pr.shape):
+        mg = "Pr must be a numpy.ndarray with the same shape as P"
+        raise TypeError(mg)
+    if np.min(Pr) < 0 or np.amax(Pr) > 1:
+        raise ValueError("All values in {P} must be in the range [0, 1] ")
+    if not np.isclose([np.sum(Pr)], [1])[0]:
+        raise ValueError("Pr must sum to 1")
+
     factor = np.math.factorial
     factNX = (factor(n) / (factor(x) * factor(n - x)))
     likelihood = factNX * (P ** x) * ((1 - P) ** (n - x))
