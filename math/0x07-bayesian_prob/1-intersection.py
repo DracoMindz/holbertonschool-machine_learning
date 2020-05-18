8afa8cfb1a6fa2fab21ebbc30a34c03eb951fd10
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-18 19:00:57 -0400

    interection

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -28,15 +28,15 @@ def intersection(x, n, P, Pr):
         raise ValueError("All values in P must be in the range [0, 1]")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
-        mg = "Pr must be a numpy.ndarray with the same shape as P"
-        raise TypeError(mg)
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.amin(Pr) < 0 or np.amax(Pr) > 1:
-        raise ValueError("All values in P must be in the range [0, 1] ")
+        raise ValueError("All values in Pr must be in the range [0, 1] ")
     if not np.isclose([np.sum(Pr)], [1])[0]:
         raise ValueError("Pr must sum to 1")
 
     factor = np.math.factorial
     factNX = (factor(n) / (factor(x) * factor(n - x)))
-    likelihood = factNX * (P ** x) * ((1 - P) ** (n - x))
+    likelihood = factNX * (P**x) * ((1 - P)**(n - x))
     return likelihood * Pr
