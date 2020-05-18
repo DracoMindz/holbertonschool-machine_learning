d920861cda86f9ffb84819e0a549c87f32ad14a7
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-18 19:09:08 -0400

    interection

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -32,7 +32,7 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.amin(Pr) < 0 or np.amax(Pr) > 1:
-        raise ValueError("All values in Pr must be in the range [0, 1] ")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(Pr.sum(), 1):
         raise ValueError("Pr must sum to 1")
 
