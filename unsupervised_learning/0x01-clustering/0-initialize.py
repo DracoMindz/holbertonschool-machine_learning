fe2edc19d398f96d7d85cdbe0e2db97a92cd1e38
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-22 15:08:31 -0400

    K-means

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -11,8 +11,6 @@ def initialize(X, k):
     Initialize cluster centroid for K-means
     :param X: np.ndarray, shape(n, d)
                 contains data set used for K-means
-    :param n: num of data points
-    :param d: num dimensions for each data point
     :param k: pos integer containing the num clusters
     :return: np.ndarray, of shape (k, d)
     """
