70fc2707ed05a50d683e02fb9291c9da83dd5d11
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-28 17:29:40 -0400

    Kmeans w/ slearn

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Function performs K-means on a data
+"""
+
+import numpy as np
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """
+    performs K-means on a dataset
+    :param X: np.ndarray, (n,d), dataset
+    :param k: num of clusters
+    C: np.ndarray, shape(k,d), centroid means of each cluster
+    clss: np.ndarray, shape(n,) index of cluxster in C
+            for
+    :return: C, clss
+    """
+    means = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+    return means.cluster_centers_, means.labels_
