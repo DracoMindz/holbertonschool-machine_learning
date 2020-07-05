33e0e67a7706f7800ff5400a3910ef74c7eed90d
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-07-05 05:40:24 +0000

    no need to account for zero

---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Fuction performs forward propagation for simple RNN
+"""
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+    performs forward propagation
+    :param rnn_cell: instance of RNNCell, used for forward propagation
+    :param X: data to be used, np.ndarray, shape(t, m, i)
+    :param h_0: initial hidden state, npndarray, shape(m, h)
+    Note: t = max num of time steps
+          m = batch size
+          i = dimensionality of the data
+    Note: h = dimensionality of hidden state
+    :return: H, Y
+        Note:   H = np.ndarray, contains hidden states
+                Y = np.ndarray, contains all the outputs
+    """
+
+    t, m, i = X.shape
+    m, h = h_0.shape
+    H = np.zeros((t+1, m, h))
+    Y = []
+    H[0] = h_0
+    for step in range(t):
+        H[step+1], y_layer = rnn_cell.forward(H[step], X[step])
+        # if step == 0:
+        # Y = y_layer
+        # else:
+        Y.append(y_layer)
+    # reshape or asarray to return np.ndarray
+    Y = np.asarray(Y)
+    return H, Y
