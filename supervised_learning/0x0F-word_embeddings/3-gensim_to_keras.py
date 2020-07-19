e7a209244112bb41202e02e30a05381dab26c989
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-07-19 12:09:20 +0000

    NLP Word Word2Vec

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+function converts a gensim word2vec model to keras Embedding layer
+"""
+import numpy as np
+import tensorflow.keras as K
+from gensim.models import Word2Vec
+
+
+def gensim_to_keras(model):
+    """
+    converts gensim word2vec model to keras embedding layer
+    :param model: trained gensim word2vec models
+    :return: trainable keras embedding
+    """
+    e_layer = K.layers.Embedding(input_dim,
+                                 output_dim,
+                                 embeddings_initializer='uniform',
+                                 embeddings_regularizer=None,
+                                 activity_regularizer=None,
+                                 embeddings_constraint=None,
+                                 mask_zero=False, input_length=None)
+
+    e_layerModel = model.wv.get_keras_embedding(train_embeddings=True)
+    return e_layerModel
