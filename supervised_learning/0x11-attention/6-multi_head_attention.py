e77ae60428ce13c567eba292e45db2cb58899778
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-07-30 20:25:46 +0000

    adjusted weights

--- supervised_learning/0x11-attention/6-multi_head_attention.py
+++ supervised_learning/0x11-attention/6-multi_head_attention.py
@@ -7,9 +7,8 @@ import tensorflow as tf
 sdp_attention = __import__('5-sdp_attention').sdp_attention
 
 
-class MultiHeadAttention(tensorflow.keras.layers.Layer):
+class MultiHeadAttention(tf.keras.layers.Layer):
     """ class"""
-
     def __init__(self, dm, h):
         """
         class constructor
@@ -29,20 +28,21 @@ class MultiHeadAttention(tensorflow.keras.layers.Layer):
         linear: Dense layer w/ dm units
             used to generate attention output
         """
+        super(MultiHeadAttention, self).__init__()
         self.h = h
         self.dm = dm
-        self.depth = self.dm // self.h
+        self.depth = int(self.dm // self.h)
         self.Wq = tf.keras.layers.Dense(dm)
         self.Wk = tf.keras.layers.Dense(dm)
         self.Wv = tf.keras.layers.Dense(dm)
         self.linear = tf.keras.layers.Dense(dm)
 
-    def splitHeads(selfself, m, batch):
+    def splitHeads(self, m, batch):
         """
         split last dim shape(self.h, self.depth)
         transpose result shape(batch, -1, self.h, self.depth)
         """
-        m = tf.reshape(x, (batch, -1, self.h, self.depth))
+        m = tf.reshape(m, (batch, -1, self.h, self.depth))
         return tf.transpose(m, perm=[0, 2, 1, 3])
 
     def call(self, Q, K, V, mask):
@@ -79,8 +79,8 @@ class MultiHeadAttention(tensorflow.keras.layers.Layer):
         output, weights = sdp_attention(Q, K, V, mask)
 
         # transpose output, reshape output,
-        output = tf.transpose(weights, perm=[0, 2, 1, 3])
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
         output = tf.reshape(output, (batch, -1, self.dm))
         output = self.linear(output)
 
-        return outputs, weights
+        return output, weights
