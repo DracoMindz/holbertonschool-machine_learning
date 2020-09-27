3b85d3999f7cf76b45e013c6f19c418784795697
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-09-27 10:28:18 +0000

    use seconds

--- pipeline/0x00-pandas/3-rename.py
+++ pipeline/0x00-pandas/3-rename.py
@@ -6,5 +6,12 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 # YOUR CODE HERE
+# change the name of the column
+df = df.rename(columns={"Timestamp": "Datetime"})
+# convert col Datetime to_datetime
+df["Datetime"] = pd.to_datetime(df["Datetime"], unit='s')
+
+# reduction in the dimensions of the returned object
+df = df.loc[:, ["Datetime", "Close"]]
 
 print(df.tail())
