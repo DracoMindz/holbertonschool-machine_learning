9d53289d96cf664f21191ef7f266ccfd5799f41d
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-09-27 19:26:14 +0000

    Concise Code

--- pipeline/0x00-pandas/5-slice.py
+++ pipeline/0x00-pandas/5-slice.py
@@ -5,6 +5,7 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = # YOUR CODE HERE
+# iloc needs a numerical position, count the columns
+df = df.iloc[::60, 2:6]
 
 print(df.tail())
