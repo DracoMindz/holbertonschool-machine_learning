f77eb6a2ba3c0b7299dae7a83fc4cfa5aad1bd37
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-09-29 15:17:59 +0000

    Launch Data

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -2,3 +2,43 @@
 """ Write script that displays the up coming launch"""
 
 import requests
+
+if __name__ == '__main__':
+    # need url
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    # request url
+    launchData = requests.get(url).json()
+    # set date type for unbound value comparison
+    date = float('inf')
+    # set for loop to iterate through launches
+    for idx, launch in enumerate(launchData):
+        if date > launch["date_unix"]:
+            date = launch["date_unix"]
+            launchIdx = idx
+    # get data based on launchData[launchIdx]
+    upLaunch = launchData[launchIdx]
+    # name
+    name = upLaunch["name"]
+    # date in local time
+    dateLocal = upLaunch["date_local"]
+    # rocket: need url, request.get, json, rocket name
+    rocket = upLaunch["rocket"]
+    rocketUrl = "https://api.spacexdata.com/v4/rockets/{}".format(rocket)
+    rocketData = requests.get(rocketUrl).json()
+    rocketName = rocketData["name"]
+
+    # launch pad: need url, request.get, json, launch pad name
+    launchPad = upLaunch["launchpad"]
+    launchPadUrl = "https://api.spacexdata.com/v4/launchpads/{}".\
+        format(launchPad)
+    launchPadData = requests.get(launchPadUrl).json()
+    launchPadName = launchPadData["name"]
+
+    # launch pad locality
+    launchPadLoc = launchPadData["locality"]
+
+    # Print name, (date in local time), rocket name,
+    # launch pad name, (lunch pad locality)
+    print("{} ({}) {} - {} ({})".format(name, dateLocal,
+                                        rocketName, launchPadName,
+                                        launchPadLoc))
---
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Write a script that displays the num of launches per rocket"""
+
+import requests
+
+
+if __name__ == '__main__':
