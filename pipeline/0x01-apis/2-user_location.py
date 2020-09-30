3952698a89a13ebb7452c04d55c50edca6cff98a
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-09-30 20:35:55 +0000

    Rockets

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -18,7 +18,31 @@ if __name__ == '__main__':
         # set value for x / 60
         limit = (int(r.headers["X-Ratelimit-Reset"]))
         x = (limit - int(time.time()))
+        x = (int(x / 60))if __name__ == '__main__':
+    # need arguement
+    url = sys.argv[1]
+    # set key:value for payload
+    payload = {'accept': 'application/vnd.github.v3+json'}
+    # request url with params=payload
+    r = requests.get(url, params=payload)
+    # status code == 403
+    if r.status_code == 403:
+        # set value for x / 60
+        limit = (int(r.headers["X-Ratelimit-Reset"]))
+        x = (limit - int(time.time()))
         x = (int(x / 60))
+        # Print "Reset in {} min.format(int(x))
+        print("Reset in {} min".format(x))
+    # status code == 404
+    if r.status_code == 404:
+        # print response "not found"
+        print("Not found")
+    # status code == 200 location= r.json()["location"]
+    if r.status_code == 200:
+        location = r.json()["location"]
+        # print location
+        print(location)
+
         # Print "Reset in {} min.format(int(x))
         print("Reset in {} min".format(x))
     # status code == 404
--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -5,3 +5,26 @@ import requests
 
 
 if __name__ == '__main__':
+    # need url
+    url = "https://api.spacexdata.com/v4/launches/"
+    rocketLaunches = {}
+    # request url
+    launchData = requests.get(url).json()
+    # set for loop to iterate through launches
+    for idx, launch in enumerate(launchData):
+        # rocket: need url, request.get, json, rocket name
+        rocket = launch["rocket"]
+        rocketUrl = "https://api.spacexdata.com/v4/rockets/{}".format(rocket)
+        rocketData = requests.get(rocketUrl).json()
+        rocketName = rocketData["name"]
+        if rocketName in rocketLaunches:
+            # advance to next
+            rocketLaunches[rocketName] = +1
+        if rocketName not in rocketLaunches:
+            rocketLaunches[rocketName] = 1
+        # sorting conditions: sort dicts
+    rocketsUp = sorted(rocketLaunches.keys(), key=lambda kv: kv[0])
+    rocketsUp = sorted(rocketsUp, key=lambda kv: kv[1], reverse=True)
+    # Print rocket name, num rockets
+    for rocketName in rocket:
+        print("{}:{}".format(rocketsUp[0], rocketsUp[1]))
